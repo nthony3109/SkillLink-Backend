7e7ef75d56244efe3b8894842641a0f38c279e61
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-12-09 15:21:40 +0100

    updated

--- src/main/java/com/skillLink/skillLink/Repo/ServiceRepo.java
+++ src/main/java/com/skillLink/skillLink/Repo/ServiceRepo.java
@@ -1,7 +1,24 @@
 package com.skillLink.skillLink.Repo;
 
-import com.skillLink.skillLink.Models.Service;
+import com.skillLink.skillLink.Models.TechnicianServiceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TechnicianServiceRepo extends JpaRepository<Service, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface ServiceRepo extends JpaRepository<TechnicianServiceModel, Long> {
+    Optional<TechnicianServiceModel> findByNameIgnoreCase(String t);
+    Optional<TechnicianServiceModel> findByName(String t);
+
+    @Query("""
+    SELECT s.name
+    FROM Technician t
+    JOIN t.serviceModels s
+    WHERE t.id = :techId
+""")
+    List<String> findServiceNamesByTechnicianId(@Param("techId") Long techId);
+
+
 }
