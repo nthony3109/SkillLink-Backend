da0d6d548d0d5e62a60296ee695861ad61e001c5
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-12-25 21:44:20 +0100

    technician fetch interface

---
+++ src/main/java/com/skillLink/skillLink/DTOs/NearByTProjection.java
@@ -0,0 +1,19 @@
+package com.skillLink.skillLink.DTOs;
+
+import java.util.List;
+
+public interface NearByTProjection {
+    Long getId();
+    String getFirstname();
+    String getLastname();
+    String getUsername();
+    String getPhone();
+    String getProfileImageUrl();
+    String getLocationName();
+    double getDistance();
+    String getBio();
+    String getServices();
+
+
+
+}
