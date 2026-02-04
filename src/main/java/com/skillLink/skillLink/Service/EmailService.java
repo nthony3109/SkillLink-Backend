f21e58d2305a036fdc395a508a5485f4bb52c0cf
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-02-04 13:58:35 +0100

    emailing service

---
+++ src/main/java/com/skillLink/skillLink/Service/EmailService.java
@@ -0,0 +1,34 @@
+package com.skillLink.skillLink.Service;
+
+import com.skillLink.skillLink.Email.EmailContent;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private  final RedisService redisService;
+    private final JavaMailSender mailSender;
+
+    public JavaMailSender sendEmail(String to, String subject, EmailContent content) {
+        // Implementation to send mail
+      try{
+          MimeMessage mimeMailMessage = mailSender.createMimeMessage();
+          MimeMessageHelper helper = new MimeMessageHelper(
+                  mimeMailMessage,
+                  MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                  "UTF-8");
+          helper.setTo(to);
+          helper.setSubject(subject);
+          helper.setText(content.EmailBody(),true);
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+        return mailSender;
+    }
+
+}
